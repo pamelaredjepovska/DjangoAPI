963645558d20a68c77b65d318c5a22b1c03ea6cc
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-12-24 12:39:31 +0100

    Create company app and model

---
+++ company/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Company
+
+# Register company model
+class CompanyAdmin(admin.ModelAdmin):
+    # Columns displayed in the admin list view
+    list_display = ('company_name', 'owner', 'number_of_employees')
+    # Enables searching for companies by these fields
+    search_fields = ('company_name', 'owner__username')
+    list_filter = ('owner',)
+
+admin.site.register(Company, CompanyAdmin)
