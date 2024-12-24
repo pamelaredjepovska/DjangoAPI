ea61437ba403622ddfe5cfa165e1cc4125ba0197
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-12-24 13:18:35 +0100

    Revert to initial migration

--- company/models.py
+++ company/models.py
@@ -6,7 +6,7 @@ class Company(models.Model):
     company_name = models.CharField(max_length=255)
     description = models.TextField()
     number_of_employees = models.PositiveIntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='company')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='companies')
 
     def __str__(self):
         return self.company_name
